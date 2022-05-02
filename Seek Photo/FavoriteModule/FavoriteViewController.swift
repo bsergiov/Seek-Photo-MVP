6380390ab06203863240f11786360526ba102852
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-05-02 22:02:45 +0200

    FavoriteViewController.swift added

---
+++ Seek Photo/FavoriteModule/FavoriteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteViewController.swift
+//  Seek Photo
+//
+//  Created by BSergio on 02.05.2022.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}

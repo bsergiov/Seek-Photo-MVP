a778b3f37315678635059cf1dddcfec6a4a9c9d3
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-05-03 01:21:44 +0200

    UIFont + Extensions.swift added

---
+++ Seek Photo/Extensions/UIKit/UIFont + Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIFont + Extensions.swift
+//  Seek Photo
+//
+//  Created by BSergio on 03.05.2022.
+//
+
+import UIKit
+
+extension UIFont {
+    static func avenirNext17() -> UIFont? {
+        UIFont(name: "Avenir Next", size: 17)
+    }
+    
+    static func avenirNext20() -> UIFont? {
+        UIFont(name: "Avenir Next", size: 20)
+    }
+    
+    static func avenirNext25() -> UIFont? {
+        UIFont(name: "Avenir Next", size: 25)
+    }
+}

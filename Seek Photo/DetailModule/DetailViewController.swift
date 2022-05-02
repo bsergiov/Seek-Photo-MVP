9509defd9c4487143cbca89cd812760523606721
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-05-02 23:04:40 +0200

    DetailViewController.swift added

---
+++ Seek Photo/DetailModule/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  Seek Photo
+//
+//  Created by BSergio on 02.05.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

0cab10dbf2dea5b3d798522ce1eb85cc58a81b29
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-05-03 02:18:48 +0200

    StorageManager.swift refactor basic crud method added

--- Seek Photo/Services/StorageManager.swift
+++ Seek Photo/Services/StorageManager.swift
@@ -5,4 +5,39 @@
 //  Created by BSergio on 02.05.2022.
 //
 
-import Foundation
+import RealmSwift
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    let realm = try! Realm()
+    
+    private init() { }
+    
+    func fetch(completion: @escaping (Results<FavoriteModel>) -> Void) {
+        let pictures = realm.objects(FavoriteModel.self)
+        completion(pictures)
+    }
+    
+    func save(_ picture: FavoriteModel) {
+        write {
+            realm.add(picture)
+        }
+    }
+    
+    func remove(_ picture: FavoriteModel) {
+        write {
+            realm.delete(picture)
+        }
+    }
+    
+    // MARK: - Helper Method
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write { completion() }
+        } catch  {
+            print("Error write realm in db: \(error)")
+        }
+    }
+}

9c45181c26d629c636c32265ac2048d5b0cd3797
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-05-03 12:10:44 +0200

    ImgCacheManager.swift refactor basic logic added for cache

--- Seek Photo/Services/ImgCacheManager.swift
+++ Seek Photo/Services/ImgCacheManager.swift
@@ -5,4 +5,26 @@
 //  Created by BSergio on 02.05.2022.
 //
 
-import Foundation
+import UIKit
+
+class ImgCacheManager {
+    
+    static var instance = ImgCacheManager()
+    
+    private var imgCache: NSCache<NSString, UIImage> = {
+        var cache = NSCache<NSString, UIImage>()
+        cache.countLimit = 100
+        cache.totalCostLimit = 1024 * 1024 * 100 // ~ 200mb
+        return cache
+    }()
+    
+    private init() { }
+    
+    func add(key: String, value: UIImage) {
+        imgCache.setObject(value, forKey: key as NSString)
+    }
+    
+    func get(key: String) -> UIImage? {
+        return imgCache.object(forKey: key as NSString)
+    }
+}
